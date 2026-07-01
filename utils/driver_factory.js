8cafe63f4bfa2239bfe1432208c07eabdc9fecc5
Parag Khare <paragkhare.14@gmail.com>
2026-07-01 21:39:58 +0530

    Added GitHub Actions workflow

--- utils/driver_factory.js
+++ utils/driver_factory.js
@@ -1,11 +1,19 @@
 const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
-const options = new chrome.Options();
-
-options.addArguments("--incognito");
 
 async function createDriver() {
+
+    const options = new chrome.Options();
+    options.addArguments("--incognito");
+
+    // Enable headless mode only in GitHub Actions
+    if (process.env.GITHUB_ACTIONS) {
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+    }
     return await new Builder()
         .forBrowser("chrome")
         .setChromeOptions(options)
